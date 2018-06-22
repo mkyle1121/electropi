3395df1ea0af1451ffc198a0b8a65016d67fdb34
Mike Kyle <mike@mike.com>
2018-06-22 16:06:11 +0000

    led push

---
+++ led.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as GPIO
+import time
+
+ledPin = 11
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(ledPin, GPIO.OUT) # set output mode
+    GPIO.output(ledPin, GPIO.LOW) # seet output to low (off)
+    print('using pin {}'.format(ledPin))
+
+def loop():
+    while True:
+        GPIO.output(ledPin, GPIO.HIGH)
+        print ("led on")
+        time.sleep(.5)
+        GPIO.output(ledPin, GPIO.LOW)
+        print("led off")
+        time.sleep(.5)
+
+def destroy():
+    GPIO.output(ledPin, GPIO.LOW)
+    GPIO.cleanup()
+
+if __name__ == "__main__":
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
+
+
